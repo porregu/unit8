354765bc118368e212cdf8a1cbb418e02f7ae367
guillermo <porregu@ssfs.org>
2019-11-15 10:12:20 -0500

    work with brian in class and start claswork

---
+++ claswork1.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root = Tk()
+
+title_label = Label(root , text="Hello, World")
+title_label.grid(row=1, column=1)
+
+
+root.mainloop()
---
+++ claswork2.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root = Tk()
+
+title_label = Label(root , text="say hello")
+title_label.grid(row=1, column=1)
+
+
+root.mainloop()
